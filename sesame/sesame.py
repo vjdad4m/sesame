39c81037e19be27c48ef525735a0562b30ac220a
Adam Vajda <vjdad4m@gmail.com>
2021-06-27 00:13:52 +0200

    Created specgram

--- sesame/sesame.py
+++ sesame/sesame.py
@@ -2,6 +2,7 @@
 # for now it will be used for testing.
 
 import numpy as np
+np.seterr(divide = 'ignore') 
 import matplotlib.pyplot as plt
 from tools import fetch
 from tools import tools
@@ -13,8 +14,14 @@ data = data.T[0]    # Remove stereo -> Getting the median is probably a better w
 print(f'{sr = }')
 print(f'{data.shape = }')
 
-data = tools.PurgeData(data, 5)         # Do not purge when fingerprinting (!) -> loss of data results in wrong fingerprints
+data = tools.PurgeData(data, 2)         # Do not purge when fingerprinting (!) -> loss of data results in wrong fingerprints
 print(f'After purge {data.shape = }')
 
 plt.plot(data)
+plt.show()
+
+plt.specgram(data, Fs = sr, NFFT=128, noverlap=64, cmap='jet')
+plt.ylabel('Frequency (Hz)')
+plt.xlabel('Time (s)')
+plt.colorbar()
 plt.show()
--- sesame/tools/tools.py
+++ sesame/tools/tools.py
@@ -1,6 +1,11 @@
 import numpy as np
+from scipy import signal
 
 def PurgeData(data, level):
     for i in range(level):
         data = np.delete(data, np.arange(0, data.size, 2))
     return data
+
+def GetSpectrogram(data, sr):
+    frequencies, times, spectrogram = signal.spectrogram(data, sr)
+    return spectrogram, frequencies, times
